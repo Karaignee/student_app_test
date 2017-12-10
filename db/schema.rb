dd10fb451c27abf0b601b29cce44a5f229ab586c
Karen Rooney <karaignee@gmail.com>
2017-12-10 15:29:30 +0000

    dropdowns part1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210145615) do
+ActiveRecord::Schema.define(version: 20171210152456) do
+
+  create_table "colleges", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.text     "name"
