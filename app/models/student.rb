ffe1e6262ece32e3f41c1992a2579af1ef3ced72
Karen Rooney <karaignee@gmail.com>
2017-12-11 07:01:07 +0000

    Revert "Validate Kyle WIP"
    This reverts commit 0a9d5caf9aa0091b9170fc1cb64519e228ca2857.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,4 @@
 class Student < ActiveRecord::Base
 
-# validates :name, format: { with: "Kyle", message: "name must be Kyle" } #error
-validates :name, presence: { message: "name must be Kyle" }
-
 belongs_to :college
 end
